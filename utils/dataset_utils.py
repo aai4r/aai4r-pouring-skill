77a934219be254d814263841ba5112297cb461a6
twkim <twkim0812@gmail.com>
2022-06-13 14:58:55 +0900

    - enlarge the robot configuration range - plot function implementation for dataset analysis

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -3,6 +3,9 @@ import h5py
 import torch
 import numpy as np
 import cv2
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+plt.ion()
 
 from spirl.utils.general_utils import AttrDict
 np.set_printoptions(precision=3)
@@ -19,6 +22,39 @@ class DatasetUtil:
 
         self.folder_list = sorted(os.listdir(self.path), key=lambda x: int(x[5:]))
         print("batch folder list: {}".format(self.folder_list))
+
+        # initialize plot
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.reset_plot()
+
+    def reset_plot(self):
+        self.ax.clear()
+        fontlabel = {"fontsize": "large", "color": "gray", "fontweight": "bold"}
+        self.ax.set_xlabel("X", fontdict=fontlabel, labelpad=16)
+        self.ax.set_ylabel("Y", fontdict=fontlabel, labelpad=16)
+        self.ax.set_xlim([-0.5, 0.5])
+        self.ax.set_ylim([-0.5, 0.5])
+        self.ax.set_zlim([-0.5, 0.5])
+        self.ax.view_init(elev=30, azim=0)
+        legend = self.ax.get_legend()
+        if legend is not None:
+            legend.remove()
+        self.px = np.array([])
+        self.py = np.array([])
+        self.pz = np.array([])
+
+    def plot3d(self, p, label='', color=''):
+        self.px = np.append(self.px, p[0])
+        self.py = np.append(self.py, p[1])
+        self.pz = np.append(self.pz, p[2])
+        self.ax.plot(self.px, self.py, self.pz, label=label, color='red')
+        if self.ax.get_legend() is None:
+            self.ax.legend()
+
+        # self.ax.scatter(p[0], p[1], p[2], color="green")
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
     def rollout_play(self):
         exit_flag = False
@@ -50,24 +86,33 @@ class DatasetUtil:
                         print("{}: shape: {}".format(name, data[name].shape))
 
                     step = 0
-                    for img, st in zip(data.images, data.states):
+                    for img, st, a in zip(data.images, data.states, data.actions):
                         if exit_flag: break
                         print("    rollout: {} step: {} / {}, img, shape: {}, min/max: {}/{}  type: {}".format(
                             int(rollout[rollout.find('_')+1:rollout.find('.')]),
                             step, len(data.images), img.shape, img.min(), img.max(), img.dtype))
+                        print("        action: joint={}, grip={}".format(a[:6], a[6:]))
                         print("        dof_pos: {}".format(st[:6]))
                         print("        grip_pos: {}".format(st[6:7]))
-                        print("        bottle_pos: {}".format(st[7:10]))
-                        print("        bottle_rot: {}".format(st[10:14]))
-                        print("        cup_pos: {}".format(st[14:17]))
-                        print("        cup_rot: {}".format(st[17:21]))
-                        print("        liq_rot: {}".format(st[21:24]))
-                        step += 1
+                        print("        grasp_pos: {}".format(st[7:10]))
+                        print("        grasp_rot: {}".format(st[10:14]))
+                        print("        cup-bottle_tip_diff: {}".format(st[14:17]))
+                        print("        bottle_pos: {}".format(st[17:20]))
+                        print("        bottle_rot: {}".format(st[20:24]))
+                        print("        cup_pos: {}".format(st[24:27]))
+                        print("        cup_rot: {}".format(st[27:31]))
+                        print("        liq_pos: {}".format(st[31:]))
+
+                        self.plot3d(p=st[7:10], label="grasp_pos")
+
                         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                         cv2.imshow(task_name, img)
                         k = cv2.waitKey(0)
                         if k == 27:
                             exit_flag = True
+                        step += 1
+
+                self.reset_plot()
 
     def statistics(self):
         frames = {}
@@ -119,6 +164,5 @@ if __name__ == '__main__':
     task_name = "pouring_water_img"      # block_stacking, pouring_water_img
 
     du = DatasetUtil(data_path=data_path, task_name=task_name)
-    du.statistics()
-    # du.rollout_play()
-
+    # du.statistics()
+    du.rollout_play()
